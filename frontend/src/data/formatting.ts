6ebc205bc6a7517788389d58d6ded0056d68b259
patrik-ha <patrikham0@gmail.com>
2024-08-04 18:35:04 +0200

    Styling hyperlinks using Tailwind

--- frontend/src/data/formatting.ts
+++ frontend/src/data/formatting.ts
@@ -45,6 +45,14 @@ function formatDate(entry: DataEntryType, key: string) {
     return moment(String(value)).format('MM/DD/YYYY')
 }
 
+// The point of this function is to return a standardized link.
+// The thing was that there were more than one entry that needed a link, and so 
+// it was not that nice to have multiple link templates essentially doing the same thing.
+function createLink(href: string | undefined, text: string | undefined) {
+    return `<a target="_blank" class="underline text-blue-800" href="${href}">${text}</a>`;
+}
+
+
 // In some cases, we might need to process the data before showing it. (i.e. parse dates)
 // In these cases, define the function here, by keying it with the prop name.
 var functionLookup: { [key: string]: Function } = {
@@ -54,9 +62,9 @@ var functionLookup: { [key: string]: Function } = {
     },
     "datePublished": formatDate,
     "name": (entry: DataEntryType, key: string) => {
-        return `<a target="_blank" href="${entry["url" as DataEntryKey]}">${entry["name" as DataEntryKey]}</a>`;
+        return createLink(entry["url" as DataEntryKey]?.toString(), entry["name" as DataEntryKey]?.toString())
     },
-    "paperLink": (entry: DataEntryType, key: string) => { return entry["paperLink"] == null ? "" : `<a target="_blank" href="${entry["paperLink" as DataEntryKey]}">paper link`; }
+    "paperLink": (entry: DataEntryType, key: string) => { return entry["paperLink"] == null ? "" : createLink(entry["paperLink" as DataEntryKey]?.toString(), "paper link"); }
 };
 
 
