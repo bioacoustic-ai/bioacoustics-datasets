f99f021a7d286eb8cb69cd93ac931c54230de54a
cangonin <celine.angonin71@gmail.com>
2024-08-05 13:08:49 +0200

    use same hyperlink style for header

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import DataTable from "../components/DataTable.vue";
 </script>
+<!--  TODO: use the createLink function -->
 <template>
     <div class="flow-root">  
         <img class="h-auto max-w-xs float-left" src=".././assets/BiocAI_image.png" width=100 alt="Logo of the BiocAI project">
@@ -9,8 +10,8 @@ import DataTable from "../components/DataTable.vue";
     <div class="text-center mb-4">
         <h1 class="about text-center mb-4 mt-4 text-4xl font-extrabold">Datasets for bioacoustics</h1>
         <p>This table presents publicly available bioacoustic datasets that can be used with machine learning.</p>
-        <p><b>Want to add a dataset?</b> Follow the instructions of this <a href="https://github.com/Cangonin/bioacoustics-datasets">README</a>file.</p>
-        <p><b>Want to contribute to make this list better?</b> Open a pull request on <a href="https://github.com/Cangonin/bioacoustics-datasets">this github repo!</a> 
+        <p><b>Want to add a dataset?</b> Follow the instructions of this <a target="_blank" class="underline text-blue-800" href="https://github.com/Cangonin/bioacoustics-datasets">README file.</a></p>
+        <p><b>Want to contribute to make this list better?</b> Open a pull request on <a target="_blank" class="underline text-blue-800" href="https://github.com/Cangonin/bioacoustics-datasets">this github repo!</a> 
         </p>
     </div>
     <DataTable></DataTable>
