7a9545c098bffda2cedc3e7cad443fa249a4696a
cangonin <celine.angonin71@gmail.com>
2024-08-08 14:42:23 +0200

    typo in repo name

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bioacoustcs-datasets/',
+  base: '/bioacoustics-datasets/',
   plugins: [
     vue(),
   ],
