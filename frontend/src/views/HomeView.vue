d91d3228cc8827cbe8e85586f06fe4b6d5c70825
patrik-ha <patrikham0@gmail.com>
2024-10-09 16:18:36 +0200

    Adapted logo for dark mode

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,8 +4,19 @@ import DataTable from "../components/DataTable.vue";
 <!--  TODO: use the createLink function -->
 <template>
     <div class="flow-root">  
-        <img class="h-auto max-w-xs float-left" src=".././assets/BiocAI_image.png" width=100 alt="Logo of the BiocAI project">
-        <img class="h-auto  max-w-xs rounded-lg float-right" src=".././assets/EU.png" width=90 alt="EU flag">
+        <div>
+            <!--
+            Cool and practical way to set the source of images dynamically:
+            https://developer.mozilla.org/en-US/docs/Web/HTML/Element/picture
+            -->
+            <picture>
+                <source srcset=".././assets/BiocAI_image.png" media="(prefers-color-scheme: light)"/>
+                <source srcset=".././assets/BiocAI_image_dark.png"  media="(prefers-color-scheme: dark)"/>
+                <!--The source here is the "default", i.e. the normal version of the logo.-->
+                <img width="100" class="h-auto max-w-xs float-left" src=".././assets/BiocAI_image.png"/>
+            </picture>
+        </div>
+        <img class="h-auto max-w-xs rounded-lg float-right" src=".././assets/EU.png" width=90 alt="EU flag">
     </div>
     <div class="text-center mb-4">
         <h1 class="about text-center mb-4 mt-4 text-4xl font-extrabold">Datasets for bioacoustics</h1>
